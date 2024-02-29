8d1243ffa1135727ce6675fd270f5dc02f0ff1ed
Brandon Wong <bwong109@usc.edu>
2024-02-29 19:58:19 +0000

    checkpoint

--- logicsim/event.h
+++ logicsim/event.h
@@ -13,7 +13,7 @@ typedef struct EventLess {
     
     //write the operator() required to make this a functor that compares Events by time
     bool operator()(const Event* event1, const Event* event2) const{
-        return event1 -> time > event2 -> time;
+        return event1 -> time < event2 -> time;
     }
 } EventLess;
 	
